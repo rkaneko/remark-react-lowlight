c327e44dd50ea046322470bf636e0392429c4639
ryota-kaneko <ryota-kaneko@cybozu.co.jp>
2016-10-20 21:43:39 +0900

    `wip` supporting inline code block

--- src/index.jsx
+++ src/index.jsx
@@ -8,18 +8,33 @@ export default (languageDefinitions) => {
     Lowlight.registerLanguage(language, definition);
   });
 
-  const Code = ({ className = '', children }) => {
+  const Code = ({ className = '', children, inline = true }) => {
     const language = className.split('-')[1] || '';
+    const child = children[0];
+    if (child.type === 'code') {
+
+    }
     const value = children[0] || '';
-    const props = { language, value };
+    const props = { language, value, inline };
     return (
       <Lowlight {...props} />
     );
   };
   Code.propTypes = {
     className: React.PropTypes.string,
-    children: React.PropTypes.node
+    children: React.PropTypes.node,
+    inline: React.PropTypes.boolean
   };
 
-  return Code;
+  const Pre = ({ children }) => {
+    const codeElements = children.filter(child => child.type === 'code');
+    if (codeElements.length === 1) {
+      const codeElement = codeElements[0];
+      const props = Object.assign({}, codeElement.props, { inline: false });
+      return <pre><Code {...props} /></pre>;
+    }
+    return <pre>{children}</pre>;
+  };
+
+  return Pre;
 };
